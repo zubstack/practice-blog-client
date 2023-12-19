aabedc312c9db0d0f52e6e2bde6f0e6067d39858
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-12-19 17:58:56 -0500

    Revert "Testing: posts"
    This reverts commit 5f9948ac792cb963bbb6ed74ef831aabc09c8a05.

--- src/components/Post.test.js
+++ src/components/Post.test.js
@@ -1,55 +1,51 @@
-import '@testing-library/jest-dom';
-import jest from 'jest';
-import { render, screen } from '@testing-library/react';
-import { prettyDOM } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import Post from '../components/Post';
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { prettyDOM } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Post from "../components/Post";
 
 const post = {
-  id: 'sjoiej23e23',
-  title: 'El cielo azul',
-  author: 'Cristian Castro',
+  id: "sjoiej23e23",
+  title: "El cielo azul",
+  author: "Cristian Castro",
   user: {
-    username: '@zubstack',
+    username: "@zubstack",
   },
-  likes: ['ramdom_user_id'],
+  likes: ["ramdom_user_id"],
 };
-const token = 'dnoifhr8349trh934fjoijerasf';
+const token = "dnoifhr8349trh934fjoijerasf";
 const fetchPosts = () => {
-  console.log('fethching posts');
+  console.log("fethching posts");
 };
 
-test('renders content', () => {
+test("renders content", () => {
   const component = render(
     <Post post={post} token={token} fetchPosts={fetchPosts} />
   );
 
-  component.getByText('El cielo azul - Cristian Castro');
-  const detailsButton = component.container.querySelector('button');
+  component.getByText("El cielo azul - Cristian Castro");
+  const detailsButton = component.container.querySelector("button");
   console.log(prettyDOM(detailsButton));
 });
 
-test('Button for show details is shown', () => {
+test("Button for show details is shown", () => {
   const { getByText, container } = render(
     <Post post={post} token={token} fetchPosts={fetchPosts} />
   );
-  expect(getByText('See details')).toBeInTheDocument();
-  const div = container.querySelector('.likes');
+  expect(getByText("See details")).toBeInTheDocument();
+  const div = container.querySelector(".likes");
   expect(div).toBeNull();
 });
 
-test('clicking the button shows us the hidden content', async () => {
+test("clicking the button shows us the hidden content", async () => {
   const { getByText, container } = render(
     <Post post={post} token={token} fetchPosts={fetchPosts} />
   );
 
-  const mockHandler = jest.fn();
   const user = userEvent.setup();
-  const button = screen.getByRole('button');
-  button.onclick = mockHandler;
+  const button = screen.getByRole("button");
   await user.click(button);
-  const div = container.querySelector('.likes');
-  expect(getByText('Hide details')).toBeInTheDocument();
+  const div = container.querySelector(".likes");
+  expect(getByText("Hide details")).toBeInTheDocument();
   expect(div).toBeDefined();
-  expect(mockHandler.mock.calls).toHaveLength(1);
 });
